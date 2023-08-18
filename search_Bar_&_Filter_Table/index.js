e9e105da3c871ba634ba618f08ac5010561313a0
vandanavnaik66 <vandanavnaik66@gmail.com>
2023-08-18 15:37:21 +0530

    Filter project added

---
+++ search_Bar_&_Filter_Table/index.js
@@ -0,0 +1,18 @@
+const searchFun=()=>{
+    let dataTyped=document.getElementById("inputId").value.toUpperCase();
+    let myTable=document.getElementById("myTable")
+    let tr=myTable.getElementsByTagName("tr")
+ for (var i=0; i<tr.length; i++){
+
+    let td=tr[i].getElementsByTagName('td')[0];
+    if(td){
+        let textValue=td.textContent || td.innerHTML;
+        if(textValue.toUpperCase().indexOf(dataTyped) >-1){
+          tr[i].style.display=""; //show the element
+        }else{
+            tr[i].style.display="none"; // hide the element
+        }
+    }
+ }
+
+}
